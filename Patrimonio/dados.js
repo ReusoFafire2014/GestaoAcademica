00f393418f92b18e0e30881df04cf60d172cf6d8
Filipe Torres <filipe.virginio.torres@gmail.com>
2014-11-17 20:14:41 -0300

    Adicao do plugin data table e da tela de consulta

---
+++ Patrimonio/dados.js
@@ -0,0 +1,44 @@
+{
+  "data": [
+    [
+      "Notebook para o setor de compras.",
+      "Dell Inspiron 1551",
+      "1",
+      "Ativo",
+      "Lognet Informática",
+      "11/07/2010"
+    ],
+    [
+      "Cadeira com 4 lugares para sala de espera.",
+      "Giroflex Quadra 4",
+      "2",
+      "Ativo",
+      "Cadeform",
+      "19/06/2010"
+    ],
+    [
+      "Mesa em L para secretaria do depto de administração",
+      "Giroflex Plana Plus",
+      "3",
+      "Ativo",
+      "Cadeform",
+      "11/06/2010"
+    ],
+    [
+      "Armário em aço para funcionários com 24 lugares",
+      "Ferroplus 2405",
+      "4",
+      "Ativo",
+      "Armazém Viana",
+      "18/05/2012"
+    ],
+    [
+      "Bancada em madeira para laboratório de informática",
+      "Giroflex Banca Plus",
+      "5",
+      "Ativo",
+      "Cadeform",
+      "21/03/2013"
+    ]
+  ]
+}
